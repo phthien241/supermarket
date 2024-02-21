3ee9eda2474bb1075434f214ccaf626d1f3240eb
huuthien241 <huuthien241@gmail.com>
2024-02-21 23:46:10 +1100

    ok

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Product } from '../models/product.model';
+import { environment } from 'src/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,6 @@ export class ProductService {
   constructor(private http: HttpClient) {}
 
   getProduct(filter: string) {
-    return this.http.post<Product[]>(`http://localhost:3000/api/product`,{filter:filter});
+    return this.http.post<Product[]>(`${environment.apiUrl}/api/product`,{filter:filter});
   }
 }
