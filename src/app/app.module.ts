9beaa2505413d30c473926b4a8dbdd38b1506efe
huuthien241 <huuthien241@gmail.com>
2024-02-21 16:22:14 +1100

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ export function MSALInstanceFactory(): PublicClientApplication {
     auth: {
       clientId: '624d74bd-acc1-487d-ab20-5c82d5da7e79',
       authority: 'https://adAzureBestBuy.b2clogin.com/adAzureBestBuy.onmicrosoft.com/B2C_1_Best-Buy-SignIn-SignUp',
-      redirectUri: 'http://localhost:4200/',
+      redirectUri: 'https://limitless-caverns-40418-b42a5a2d3917.herokuapp.com/',
       knownAuthorities: ['adAzureBestBuy.b2clogin.com']
     },
     cache: {
